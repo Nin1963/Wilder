309f18e26d20eac3a53a2594ed3ea2f083d16b52
Nin1963 <ninayamina1963@gmail.com>
2019-09-25 12:05:10 +0200

    WilderClassroom

---
+++ src/wilder/Classroom.java
@@ -0,0 +1,13 @@
+package wilder;
+
+public class Classroom {
+	
+	    public static void main(String[] args) {
+	        Wilder prenom = new Wilder("Nina", true);
+	        Wilder prenom2 = new Wilder("Yamina", false);
+
+	        prenom.whoAmI();
+	        prenom2.whoAmI();
+	    }
+	}
+
